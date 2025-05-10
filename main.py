dff68e53e23e4085bbe34edb0145f054adac9fdd
Tharindu Nimsara <tharinduonline1080@gmail.com>
2025-05-10 14:19:16 +0530

    made respose structure

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 
 load_dotenv()
 
@@ -16,10 +17,10 @@ class ResearchResponse(BaseModel):
     tools_used: list[str] 
 
 #choosing the llm
-llm = ChatAnthropic(model="claude-3-5-sonnet-20241022")
-parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+my_llm = ChatAnthropic(model="claude-3-5-sonnet-20241022")
+my_parser = PydanticOutputParser(pydantic_object=ResearchResponse)
 
-prompt = ChatPromptTemplate.from_messages(
+my_prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
@@ -33,5 +34,28 @@ prompt = ChatPromptTemplate.from_messages(
         ("human", "{query}"),
         ("placeholder", "{agent_scratchpad}"),
     ]
-).partial(format_instructions=parser.get_format_instructions())
+).partial(format_instructions=my_parser.get_format_instructions())
 #this format_instruction consist of response template which we created above and now we pass it to the above prompt . 
+
+
+my_agent = create_tool_calling_agent(
+    llm=my_llm,
+    prompt=my_prompt,
+    tools=[]
+)
+
+agent_executer = AgentExecutor(agent=my_agent, tools=[],verbose=True)
+raw_response = agent_executer.invoke({{"query":"what is the temperature of sun"}})
+print(raw_response)
+
+try:
+    structured_respose = parser.parse(raw_response.get("output")[0]["text"])
+    print(structured_respose)
+except Exception as e:
+    print("Error in parsing response",e,"Raw response - ", raw_response)    
+
+
+
+
+
+
