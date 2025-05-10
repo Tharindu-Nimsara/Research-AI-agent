bf18b09bf124d74570209a304153747b734a1687
Tharindu Nimsara <tharinduonline1080@gmail.com>
2025-05-10 13:46:35 +0530

    made a response template

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
 
 load_dotenv()
 
+#we get repsonse templates from BaseModel of Pydantic 
+#this is a schema
+class ResearchResponse(BaseModel):
+    topic:str
+    summary: str
+    sources: list[str]
+    tools_used: list[str] 
+
+#choosing the llm
 llm = ChatAnthropic(model="claude-3-5-sonnet-20241022")
-response = llm.invoke("what is the meaning of life?")
-print(response)
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
